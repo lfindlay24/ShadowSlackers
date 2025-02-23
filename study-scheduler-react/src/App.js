61dc7b6f5a3a5f364cda7deca3d715ac86cdd253
Nick-Paxton <npaxton@student.neumont.edu>
2025-02-23 14:25:37 -0700

    Added tailwindcss and added Login and Signup Pages

--- study-scheduler-react/src/App.js
+++ study-scheduler-react/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './css/App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Auth from './components/Auth';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Auth type="login" />} />
+        <Route path="/signup" element={<Auth type="signup" />} />
+      </Routes>
+    </Router>
   );
 }
 
